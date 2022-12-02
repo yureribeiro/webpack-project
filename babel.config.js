f732368562b35af1f12196a776208fbbba9aebcd
Yure Ribeiro <yureribeirocontato@gmail.com>
2022-12-01 23:34:20 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env']
+  ]
+}
+
+//não vamos utilizar a cli (liha de comando) do bael, ent deixamos esta condiguração.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/index.css'
+
+const hello = () => console.log(`It's Works ${1 + 1}`)
+  
+
+hello()
